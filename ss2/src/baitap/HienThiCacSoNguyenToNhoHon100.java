3c03f82080027f6778af4b4f3eff0dff082d5614
Kadb96 <ptvu.2838@gmail.com>
2024-11-29 21:04:13 +0700

    nop bai tap

---
+++ ss2/src/baitap/HienThiCacSoNguyenToNhoHon100.java
@@ -0,0 +1,21 @@
+package baitap;
+
+public class HienThiCacSoNguyenToNhoHon100 {
+    public static void main(String[] args) {
+        int numb = 2;
+        boolean flag = true;
+        while (numb < 100) {
+            flag = true;
+            for (int i = 2; i < numb; i++) {
+                if (numb % i == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                System.out.println(numb);
+            }
+            numb++;
+        }
+    }
+}
