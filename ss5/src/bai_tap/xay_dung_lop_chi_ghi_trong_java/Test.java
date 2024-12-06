6b7ec72eb38963f25a32db5566aed4cb2366bc62
Kadb96 <ptvu.2838@gmail.com>
2024-12-06 20:24:36 +0700

    nop bai tap

---
+++ ss5/src/bai_tap/xay_dung_lop_chi_ghi_trong_java/Test.java
@@ -0,0 +1,18 @@
+package bai_tap.xay_dung_lop_chi_ghi_trong_java;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Student student1 = new Student();
+        System.out.println("----Before set name and classes----");
+        System.out.println(student1.toString());
+        System.out.println("----Set name----");
+        student1.setName(in.nextLine());
+        System.out.println("----Set classes----");
+        student1.setClasses(in.nextLine());
+        System.out.println("----After set name and classes----");
+        System.out.println(student1.toString());
+    }
+}
