7bfc42865e546bd52313524f08c00d581823f212
Kadb96 <ptvu.2838@gmail.com>
2024-12-11 01:08:39 +0700

    nop bai tap

---
+++ ss6/src/bai_tap/loppointvamoveablepoint/Point.java
@@ -0,0 +1,44 @@
+package bai_tap.loppointvamoveablepoint;
+
+public class Point {
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    public Point(float x, float y) {
+        this.setX(x);
+        this.setY(y);
+    }
+
+    public Point() {
+
+    }
+
+    public float getX() {
+        return this.x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return this.y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setXY(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float[] getXY() {
+        return new float[]{this.x, this.y};
+    }
+
+    public String toString() {
+        return "(" + this.getX() + ", " + this.getY() + ")";
+    }
+}
---
+++ ss6/src/bai_tap/loppointvamoveablepoint/Run.java
@@ -0,0 +1,34 @@
+package bai_tap.loppointvamoveablepoint;
+
+public class Run {
+    public static void main(String[] args) {
+        Point point = new Point(1,2);
+        System.out.println(point);
+        System.out.println(point.getX());
+        System.out.println(point.getY());
+        point.setX(5);
+        point.setY(10);
+        System.out.println(point);
+        point.setXY(3,4);
+        System.out.println(point);
+        System.out.println(point.getXY()[0]);
+        System.out.println(point.getXY()[1]);
+        MoveablePoint moveablePoint = new MoveablePoint(1,2,3,4);
+        System.out.println(moveablePoint);
+        moveablePoint.setX(4);
+        moveablePoint.setY(3);
+        moveablePoint.setXSpeed(2);
+        moveablePoint.setYSpeed(1);
+        System.out.println(moveablePoint);
+        System.out.println(moveablePoint.getX());
+        System.out.println(moveablePoint.getY());
+        System.out.println(moveablePoint.getXSpeed());
+        System.out.println(moveablePoint.getYSpeed());
+        moveablePoint.setSpeed(6,7);
+        System.out.println(moveablePoint);
+        System.out.println(moveablePoint.getSpeed()[0]);
+        System.out.println(moveablePoint.getSpeed()[1]);
+        moveablePoint.move();
+        System.out.println(moveablePoint);
+    }
+}
