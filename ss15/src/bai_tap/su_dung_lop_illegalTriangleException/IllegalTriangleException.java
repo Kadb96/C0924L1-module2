490b1108096ae333bb7b4e26af09c6aaa01250cc
Kadb96 <ptvu.2838@gmail.com>
2025-01-06 01:37:18 +0700

    nop bai tap ss14

---
+++ ss15/src/bai_tap/su_dung_lop_illegalTriangleException/IllegalTriangleException.java
@@ -0,0 +1,7 @@
+package bai_tap.su_dung_lop_illegalTriangleException;
+
+public class IllegalTriangleException extends Exception {
+    public IllegalTriangleException(String message) {
+        super(message);
+    }
+}
---
+++ ss15/src/bai_tap/su_dung_lop_illegalTriangleException/Run.java
@@ -0,0 +1,40 @@
+package bai_tap.su_dung_lop_illegalTriangleException;
+
+import java.util.Scanner;
+
+public class Run {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        boolean flag = true;
+        float a = 0;
+        float b = 0;
+        float c = 0;
+        while (flag) {
+            System.out.println("Input first side of triangle");
+            a = Float.parseFloat(in.nextLine());
+            System.out.println("Input second side of triangle");
+            b = Float.parseFloat(in.nextLine());
+            System.out.println("Input third side of triangle");
+            c = Float.parseFloat(in.nextLine());
+            try {
+                isTriangleIsIllegal(a, b, c);
+                flag = false;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid Input");
+            } catch (IllegalTriangleException e) {
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Triangle is successfully created with 3 sides: " + a + b + c);
+    }
+
+    public static void isTriangleIsIllegal(float a, float b, float c) throws IllegalTriangleException {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new IllegalTriangleException("All sides must > 0");
+        } else if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new IllegalTriangleException("Can't make triable with sides: " + a + ", " + b + ", " + c);
+        }
+    }
+}
